4ea16da1a2dd79a8bcffbc63fdb760818ba01aad
jayesh nair <compskian@gmail.com>
2020-11-01 13:53:50 +0530

    "Test cases implemented"

--- src/com/jay/BankAccount.java
+++ src/com/jay/BankAccount.java
@@ -4,11 +4,17 @@ public class BankAccount {
     private String firstName;
     private String lastName;
     private double balance;
+    private int accountType;
 
-    public BankAccount(String firstName, String lastName, double balance){
+    public static final int SAVINGS = 1;
+    public static final int  CHECKING = 2;
+
+
+    public BankAccount(String firstName, String lastName, double balance, int accountType){
         this.firstName= firstName;
         this.lastName= lastName;
         this.balance = balance;
+        this.accountType = accountType;
     }
 
     //Function to add deposit amount to balance and return updated balance.
@@ -19,11 +25,19 @@ public class BankAccount {
         return balance;
     }
 
+    public boolean isChecking(){
+        return accountType == CHECKING;
+    }
+
+    public boolean isSavings(){
+        return accountType == SAVINGS;
+    }
+
     //Function to withdraw  amount from balance and return updated balance.
     //if deposit is done from a bank branch , branch flag = 1
     //if deposit is done from an ATM, branch flag = 0
     public double withdraw(double amount, boolean branch){
-        balance += amount;
+        balance -= amount;
         return balance;
     }
 
--- src/com/jay/BankAccountTest.java
+++ src/com/jay/BankAccountTest.java
@@ -6,16 +6,41 @@ class BankAccountTest {
 
     @org.junit.jupiter.api.Test
     void deposit() throws Exception{
-        fail("This test is yet to be implemented");
+        BankAccount account= new BankAccount("john", "lennon", 1000, BankAccount.SAVINGS);
+        double balance = account.deposit(200, true);
+        assertEquals(1200, balance);
     }
 
     @org.junit.jupiter.api.Test
     void withdraw() throws Exception {
-        fail("This test is yet to be implemented");
+        BankAccount account= new BankAccount("john", "lennon", 1000, BankAccount.SAVINGS);
+        double balance = account.withdraw(200, true);
+        assertEquals(800, balance);
     }
 
     @org.junit.jupiter.api.Test
-    void getBalance() throws Exception {
-        fail("This test is yet to be implemented");
+    void getBalance_deposit() throws Exception {
+        BankAccount account= new BankAccount("john", "lennon", 1000,BankAccount.SAVINGS);
+        account.deposit(200, true);
+        assertEquals(1200, account.getBalance());
+    }
+
+    @org.junit.jupiter.api.Test
+    void getBalance_withdraw() throws Exception {
+        BankAccount account= new BankAccount("john", "lennon", 1000,BankAccount.SAVINGS);
+        account.withdraw(200, true);
+        assertEquals(800, account.getBalance());
+    }
+
+    @org.junit.jupiter.api.Test
+    void isChecking_true() throws Exception{
+        BankAccount account = new BankAccount("john", "lennon", 1000, BankAccount.CHECKING);
+        assertTrue(account.isChecking());
+    }
+
+    @org.junit.jupiter.api.Test
+    void isSavings_true() throws Exception{
+        BankAccount account = new BankAccount("john", "lennon", 1000, BankAccount.SAVINGS);
+        assertTrue(account.isSavings());
     }
 }
